f021831043d20d04c80fd313230f974f7d217b55
Ugo DA SILVA <ugodasilva67@gmail.com>
2022-10-19 15:43:00 +0200

    var_dump fetch all

--- appli/db-functions.php
+++ appli/db-functions.php
@@ -8,13 +8,13 @@ try {
     $db = new PDO("mysql:host=$servername;dbname=store", $username, $password, $options);
         
         $sql='SELECT * FROM product';
-        
         $results = $db->query($sql);
 
         // FETCH ALL
         $products = $results->fetchAll(PDO::FETCH_ASSOC);
         // $results->closeCursor(); -> permet de fermer le SELECT, inutile si on veux modifier la base de données.
 
+        var_dump($products);
         //FETCH
         foreach($products as $product) {
             echo '<p>'.$product['name'].'</p>';
@@ -24,7 +24,7 @@ try {
 
     } catch(PDOException $e) {
 
-//quand une erreur est rencontrée, elle est tansmise sous forme de message
+//quand une erreur est rencontrée, elle est transmise sous forme de message
     echo "Connection failed: ".$e->getMessage();
     }
 
