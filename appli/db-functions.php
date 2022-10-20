aa867d51e7e662e4ec532e547c4ab029b9ca6347
UgoDASILVA <ugodasilva67@gmail.com>
2022-10-20 09:45:26 +0200

    add function sql by ID (test)

--- appli/db-functions.php
+++ appli/db-functions.php
@@ -5,28 +5,34 @@ require 'db-config.php';
 
 //connect PDO 
 try {
-    $db = new PDO($dsn, $username, $password, $options);
-        
-        $sql='SELECT * FROM product';
-        $results = $db->query($sql);
-
-        // FETCH ALL
-        $products = $results->fetchAll(PDO::FETCH_ASSOC);
-        // $results->closeCursor(); -> permet de fermer le SELECT, inutile si on veux modifier la base de données.
-
-        var_dump($products);
-        //FETCH
-        foreach($products as $product) {
-            echo '<p>'.$product['name'].'</p>';
-            echo '<p>'.$product['description'].'</p>';
-            echo '<p>'.$product['price'].'</p>';
-        }
-
+    // fonction connexion()
+    $db = new PDO($dsn, $username, $password, $options);       
     } catch(PDOException $e) {
-
 //quand une erreur est rencontrée, elle est transmise sous forme de message
     echo "Connection failed: ".$e->getMessage();
     }
+
+    // FETCH ALL | fonction findAll()
+    $sql = $db->prepare('SELECT * FROM product');
+    $sql->execute();
+    $findAll = $sql->fetchAll(PDO::FETCH_ASSOC);
+    // affichage du FETCH ALL
+    foreach($findAll as $findOne) {
+        echo '<h2>'.$findOne['name'].'</h2>';
+        echo '<p>'.$findOne['description'].' '.$findOne['price'].' € </p>';
+    }
+
+
+        // fonction findOneById()
+        $sql1 = $db->prepare('SELECT * FROM product WHERE "id" = ? ');
+        $sql1->bindParam("id", $id);
+        $id = 1;
+        $sql1->execute();
+        $findOneById = $sql1->fetch();
+        // affichage du findOneById()
+        echo '<h2>'.$findOneById['name'].'</h2>';
+        echo '<p>'.$findOneById['description'].' '.$findOneById['price'].' € </p>';
+
 
 
 ?>
